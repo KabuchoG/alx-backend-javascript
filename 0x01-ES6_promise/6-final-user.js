98e94ec43c3cc7534823bbd68138cf9ed0837f62
Kabucho G <kabuchogeorge3@gmail.com>
2023-03-21 14:02:20 -0700

    updates

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,33 +1,17 @@
 import { signUpUser } from "./4-user-promise";
-// import { uploadPhoto } from "./5-photo-reject";
+import { uploadPhoto } from "./5-photo-reject";
 
-// export function handleProfileSignup(firstName, lastName, fileName) {
-//   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-//     .then((result) => {
-//       return result.map((res) => {
-//         [
-//           {
-//             status: res.status,
-//             value: res.status === "fulfilled" ? res.value : res.reason,
-//           },
-//         ];
-//       });
-//     })
-//     .catch(new Error("Unable to resolve."));
-// }
-
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+export function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => {
+      return result.map((res) => {
+        [
+          {
+            status: res.status,
+            value: res.status === "fulfilled" ? res.value : res.reason,
+          },
+        ];
+      });
+    })
+    .catch(new Error("Unable to resolve."));
 }
