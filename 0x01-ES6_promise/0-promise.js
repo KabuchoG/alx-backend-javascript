c92f066f5da3d3116770fa70a9045b4baae2cc2f
Kabucho G <kabuchogeorge3@gmail.com>
2023-03-23 12:35:22 -0700

    code correction

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    /*eslint-disable */
-      if (true) {
-        resolve();
-      } else {
-        reject();
-      }
-    });
-  }
+    // eslint-disable-next-line no-constant-condition
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
