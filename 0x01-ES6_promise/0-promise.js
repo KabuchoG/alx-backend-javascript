b9c6dfdcca0d9b91157bb5baf026531edc6b5118
Kabucho G <kabuchogeorge3@gmail.com>
2023-03-23 12:24:18 -0700

    elsint

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,10 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      /*eslint-disable */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    /*eslint-disable */
       if (true) {
         resolve();
       } else {
         reject();
       }
-      /* eslint-enable */
     });
   }
-  
-  export default getResponseFromAPI;
