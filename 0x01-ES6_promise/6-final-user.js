a162a7988e61227d3aa4748ce9f200d443754b0b
Kabucho G <kabuchogeorge3@gmail.com>
2023-03-23 14:32:35 -0700

    correct map function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,11 +5,10 @@ import { uploadPhoto } from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => result.map((res) => {
-      const feedback = {
+    .then((result) => result.map((res) => (
+      {
         status: res.status,
         value: res.status === 'fulfilled' ? res.value : res.reason,
-      };
-      return feedback;
-    }));
+      }
+    )));
 }
