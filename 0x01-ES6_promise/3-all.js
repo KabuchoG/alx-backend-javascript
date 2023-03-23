eb0dccd1e59cc79247aa3d6b36f93be6c15fa088
Kabucho G <kabuchogeorge3@gmail.com>
2023-03-23 12:46:35 -0700

    return promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
       const [photo, user] = result;
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
-      Error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
