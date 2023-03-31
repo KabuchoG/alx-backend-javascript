652eecdf8c978f5a7c2f622fb684be6f847863d0
Kabucho G <kabuchogeorge3@gmail.com>
2023-03-31 07:06:20 -0700

    corrections

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,10 @@
 export default function cleanSet(set, startString) {
+  // Create a new Set containing only elements of `set` that start with `startString`
   const newSet = new Set([...set].filter((val) => val.startsWith(startString)));
+  // Map over the elements of `newSet`, slicing off `startString` and joining with a dash separator
   return [...newSet].map((val) => val.slice(startString.length)).join('-');
 }
+
+// Call `cleanSet` twice with different inputs and log the results to the console
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
